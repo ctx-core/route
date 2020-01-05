450e785956223fec21cbff902363a0c371040e6d
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:04:10 -0500

    feat: store: b__ factories
    ∋ "@ctx-core/object": "^6.5.0"

--- store.d.ts
+++ store.d.ts
@@ -1,2 +1,4 @@
+export declare const b__route: (ctx?: any, opts?: any) => import("svelte/store").Writable<string>;
 export declare const __route: import("svelte/store").Writable<string>;
+export declare const b__query__route: (ctx?: any, opts?: any) => import("svelte/store").Writable<{}>;
 export declare const __query__route: import("svelte/store").Writable<{}>;
--- store.ts
+++ store.ts
@@ -1,5 +1,10 @@
 import { writable } from 'svelte/store'
 // @ts-ignore
 import { subscribe__debug } from '@ctx-core/store'
-export const __route = writable('')
-export const __query__route = writable({})
+import { _b } from '@ctx-core/object'
+export const b__route = _b('__route', ()=>
+	writable(''))
+export const __route = b__route()
+export const b__query__route = _b('__query__route', ()=>
+	writable({}))
+export const __query__route = b__query__route()
