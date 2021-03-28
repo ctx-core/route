60ac3ed06ae6ca4f9886bc136ae698dae7e1252e
Brian Takita <brian.takita@gmail.com>
2021-03-28 18:14:12 -0400

    update dependencies

--- src/route_b.d.ts
+++ src/route_b.d.ts
@@ -1,2 +1,2 @@
-export declare const route_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Writable<string>, object>;
+export declare const route_b: import("@ctx-core/object").Be<import("@ctx-core/store").Writable<string>, object>;
 export { route_b as b__route };
--- src/route_query_b.d.ts
+++ src/route_query_b.d.ts
@@ -1,2 +1,2 @@
-export declare const route_query_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Writable<{}>, object>;
+export declare const route_query_b: import("@ctx-core/object").Be<import("@ctx-core/store").Writable<{}>, object>;
 export { route_query_b as b__query__route, };
