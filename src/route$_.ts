f183e04962be22149be70e216aa166cd8af7022c
Brian Takita <brian.takita@gmail.com>
2022-01-31 18:29:14 -0600

    inlined const key

--- src/route$_.ts
+++ src/route$_.ts
@@ -1,6 +1,5 @@
 import { atom$, WritableAtom$ } from '@ctx-core/nanostores'
 import { B, be_ } from '@ctx-core/object'
-const key = 'route$'
-export const route$_:B<route$_T> = be_(key, ()=>
+export const route$_:B<route$_T> = be_('route$', ()=>
 	atom$(''))
 export type route$_T = WritableAtom$<string>
--- src/route_query$_.ts
+++ src/route_query$_.ts
@@ -1,6 +1,5 @@
 import { atom$, WritableAtom$ } from '@ctx-core/nanostores'
 import { B, be_ } from '@ctx-core/object'
-const key = 'route_query$'
-export const route_query$_:B<route_query$_T> = be_(key, ()=>
+export const route_query$_:B<route_query$_T> = be_('route_query$', ()=>
 	atom$({}))
 export type route_query$_T = WritableAtom$<any>
