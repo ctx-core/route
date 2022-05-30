f2c0111a15bac8318e169d3cf125b391275e67b8
Brian Takita <brian.takita@gmail.com>
2022-05-30 01:40:27 -0500

    update dependencies

--- src/route$_.ts
+++ src/route$_.ts
@@ -1,5 +1,5 @@
-import { atom$, WritableAtom$ } from '@ctx-core/nanostores'
+import { atom_, WritableAtom_ } from '@ctx-core/nanostores'
 import { B, be_ } from '@ctx-core/object'
 export const route$_:B<route$_T> = be_('route$', ()=>
-	atom$(''))
-export type route$_T = WritableAtom$<string>
+	atom_(''))
+export type route$_T = WritableAtom_<string>
--- src/route_query$_.ts
+++ src/route_query$_.ts
@@ -1,5 +1,5 @@
-import { atom$, WritableAtom$ } from '@ctx-core/nanostores'
+import { atom_, WritableAtom_ } from '@ctx-core/nanostores'
 import { B, be_ } from '@ctx-core/object'
 export const route_query$_:B<route_query$_T> = be_('route_query$', ()=>
-	atom$({}))
-export type route_query$_T = WritableAtom$<any>
+	atom_({}))
+export type route_query$_T = WritableAtom_<any>
