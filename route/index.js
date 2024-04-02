a1ae37eccff4885d9b4382e064d2ac089d30eca9
Brian Takita <brian.takita@gmail.com>
2024-04-01 23:00:24 -0400

    # @ctx-core/route
    ## 16.0.103
    
    ### Patch Changes
    
    - ctx-core: ^7.0.2 -> ^7.1.0
    
    ## 16.0.102
    
    ### Patch Changes

--- route/index.js
+++ route/index.js
@@ -1,12 +1,12 @@
 /// <reference types="ctx-core" />
-import { be_sig_triple_ } from 'ctx-core/rmemo'
+import { id_be_sig_triple_ } from 'ctx-core/rmemo'
 /** @typedef {be_sig_triple_T} */
 export const [
 	route$_,
 	route_,
 	route__set,
 ] = /** @type {be_sig_triple_T<string>} */
-	be_sig_triple_(()=>
-		'',
-	{ id: 'route' })
+	id_be_sig_triple_(
+		'route',
+		()=>'')
 export { route$_ as route__ }
--- route_query/index.js
+++ route_query/index.js
@@ -1,12 +1,12 @@
 /// <reference types="ctx-core" />
-import { be_sig_triple_ } from 'ctx-core/rmemo'
+import { id_be_sig_triple_ } from 'ctx-core/rmemo'
 /** @typedef {be_sig_triple_T} */
 export const [
 	route_query$_,
 	route_query_,
 	route_query__set,
 ] = /** @type {be_sig_triple_T<route_query__T>} */
-	be_sig_triple_(()=>(
-		{}),
-	{ id: 'route_query' })
+	id_be_sig_triple_(
+		'route_query',
+		()=>({}))
 export { route_query$_ as route_query__ }
